7c2b39ebd7f5c05767c8bcca43e81b1bbbb0a8eb
m3ll3m <melanielueft@yahoo.de>
2016-06-01 14:29:49 +0200

    fix current time

--- Script/script.js
+++ Script/script.js
@@ -1,11 +1,11 @@
 setInterval(function time(){
   var d = new Date();
-  var hours = 24 - d.getHours();
-  var min = 60 - d.getMinutes();
+  var hours = d.getHours();
+  var min = d.getMinutes();
   if((min + '').length == 1){
     min = '0' + min;
   }
-  var sec = 60 - d.getSeconds();
+  var sec = d.getSeconds();
   if((sec + '').length == 1){
         sec = '0' + sec;
   }
